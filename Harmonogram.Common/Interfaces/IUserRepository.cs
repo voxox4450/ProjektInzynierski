da06513b6f22c1276313cd791168ad59ca6d5aae
Jan Krzeczowski <janekkrzeczo@gmail.com>
2024-12-21 16:32:11 +0100

    changed Interfaces of User and deleted unimportant methods in UserService

--- Harmonogram.Common/Interfaces/IUserRepository.cs
+++ Harmonogram.Common/Interfaces/IUserRepository.cs
@@ -4,12 +4,10 @@ namespace Harmonogram.Common.Interfaces
 {
     public interface IUserRepository
     {
+        User GetById(int id);
         User Login(User user);
         void Register(User user);
         void Reload();
-
-        User GetById(int id);
-
         User CheckFirst(User user);
         void Update(User user);
         IEnumerable<User> GetAll();
